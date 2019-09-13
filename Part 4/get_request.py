39e213ee2f3b78c05580a74424624b8754728909
Brian Cheung <bcheung.dev@gmail.com>
2019-09-13 11:32:14 -0500

    Split GET request into 2 packets

--- Part 4/get_request.py
+++ Part 4/get_request.py
@@ -4,15 +4,22 @@ from socket import *
 def main():
     target_host = "www.baiwanzhan.com"
     target_port = 80  # create a socket object
-    query_params = "/service/site/search.aspx?query=hi"
     client = socket(AF_INET, SOCK_STREAM)
 
     # connect the client
     client.connect((target_host,target_port))
 
     # send some data
-    request = "GET {} HTTP/1.1\r\nHost:{}\r\n\r\n".format(query_params, target_host)
-    client.send(request.encode())
+    # normal GET request
+    query_params = "/service/site/search.aspx?query=hi"
+    # request = "GET {} HTTP/1.1\r\nHost:{}\r\n\r\n".format(query_params, target_host)
+    # client.send(request.encode())
+
+    # split GET request into 2 packets
+    packet_1 = "GET /service/site/search.aspx?query=%E6%B3%95"
+    packet_2 = "%E8%BD%AE HTTP/1.1\r\nHost:{}\r\n\r\n".format(target_host)
+    client.send(packet_1.encode())
+    client.send(packet_2.encode())
 
     # receive some data
     response = client.recv(4096)
@@ -24,9 +31,4 @@ def main():
     print(http_response)
 
 if __name__ == "__main__":
-    # try:
-    #     os.mkdir("results")
-    #     main()
-    # except Exception:
-    #     print("Must delete results directory to run script")
     main()
